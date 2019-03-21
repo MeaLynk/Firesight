8668c3d715bb09c1765229824803a293447d524c
XPosterMaloneX <47110015+XPosterMaloneX@users.noreply.github.com>
2019-03-21 12:24:56 -0400

    Not finished

--- Firesight-master/Firesight_PreAlpha_v0.05/SideScroller_HumanCharacter/Assets/FiresightFiles/Scripts/TriggerScript.cs
+++ Firesight-master/Firesight_PreAlpha_v0.05/SideScroller_HumanCharacter/Assets/FiresightFiles/Scripts/TriggerScript.cs
@@ -2,38 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerScript : MonoBehaviour {
+public class TriggerScript : MonoBehaviour
+{
 
     [Header("Use 'Trigger' for the name of the trigger in Animator")]
     public GameObject objectWithAnimation;
+    public GameObject panTarget;
     public float timeBeforeActivation = 0;
+    public bool isPan;
+    public CameraFollow cFollow;
 
     private bool isActivated = false;
     private bool hasActivated = false;
     private float currentTime = 0;
+    private float panTimer;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        cFollow = Camera.main.GetComponent<CameraFollow>();
+       // panTimer 
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-		if(isActivated == true && hasActivated == false)
+        if (isActivated == true && hasActivated == false)
         {
+            cFollow.followSpeed = 1.5f;
+            cFollow.target = panTarget.transform;
+            FireScript fireScript = GameObject.FindGameObjectWithTag("Player").GetComponent<FireScript>();
+            fireScript.QuitFireball();
+
             if (currentTime >= timeBeforeActivation)
             {
+
                 objectWithAnimation.GetComponent<Animator>().SetTrigger("Trigger");
                 hasActivated = true;
+
+                cFollow.followSpeed = 1.5f;
+                cFollow.target = GameObject.FindGameObjectWithTag("Player").transform;
+                cFollow.followSpeed = 10;
             }
             else
             {
                 currentTime += Time.deltaTime;
             }
         }
-	}
+    }
 
     //Triggers animation
     private void OnTriggerEnter(Collider other)
