f17b211a8a59849b2b1e80dc989d111e4a6c4d7d
XPosterMaloneX <47110015+XPosterMaloneX@users.noreply.github.com>
2019-03-31 16:40:13 -0400

    MWAHAHAHA

--- Firesight-master/Firesight_PreAlpha_v0.05/SideScroller_HumanCharacter/Assets/FiresightFiles/Scripts/DeathScript.cs
+++ Firesight-master/Firesight_PreAlpha_v0.05/SideScroller_HumanCharacter/Assets/FiresightFiles/Scripts/DeathScript.cs
@@ -1,65 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class DeathScript : MonoBehaviour {
+public class DeathScript : MonoBehaviour
+{
 
     [Header("Objects that will reset after death")]
-    public GameObject[] presurePlates;
-    public GameObject[] doors;
-    public GameObject[] keys;
-    public GameObject[] hints;
-    public GameObject[] burnableRopes;
+    public GameObject[] pressurePlates;
     //maybe add reset animations trigger if it is needed
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-		
-	}
+
+    }
 
     //Resets the all gameobjects to their default states
     public void ResetObjects()
     {
-        for (int i = 0; i < presurePlates.Length; i++)
+        for (int i = 0; i < pressurePlates.Length; i++)
         {
-            if (presurePlates[i] != null)
+            if (pressurePlates[i] != null)
             {
-                presurePlates[i].GetComponent<PressurePlate>().ResetPresurePlate();
-            }
-        }
-        for (int i = 0; i < doors.Length; i++)
-        {
-            if (doors[i] != null)
-            {
-                doors[i].GetComponent<Door>().ResetDoor();
-            }
-        }
-        for (int i = 0; i < keys.Length; i++)
-        {
-            if (keys[i] != null)
-            {
-                keys[i].GetComponent<Key>().ResetKey();
-            }
-        }
-        for (int i = 0; i < hints.Length; i++)
-        {
-            if (hints[i] != null)
-            {
-                hints[i].GetComponent<Hint>().ResetHint();
-            }
-        }
-        for (int i = 0; i < burnableRopes.Length; i++)
-        {
-            if (burnableRopes[i] != null)
-            {
-                burnableRopes[i].GetComponent<BurnRope>().ResetRopes();
+                pressurePlates[i].GetComponent<PressurePlate>().ResetPresurePlate();
             }
         }
     }
--- Firesight-master/Firesight_PreAlpha_v0.05/SideScroller_HumanCharacter/Assets/FiresightFiles/Scripts/PressurePlate.cs
+++ Firesight-master/Firesight_PreAlpha_v0.05/SideScroller_HumanCharacter/Assets/FiresightFiles/Scripts/PressurePlate.cs
@@ -3,36 +3,22 @@
 public class PressurePlate : MonoBehaviour
 {
     #region Public Variables
-    public GameObject arrowPrefab;
-    public GameObject spikesPrefab;
-    public enum EffectTypes { SHOOT_ARROWS_FROM_LEFT, SHOOT_ARROWS_FROM_RIGHT, SPIKES, OPEN_DOOR, NONE };
-    public EffectTypes effect;      //the effect the pressure plate will have
-    public int arrowAmount;
     public float lowerAmount = 11.0f;                  //the amount to lower the pressure plate by
     public float lowerSpeed = 55.0f;
     #endregion
 
     #region Private Variable
-    private GameObject[] arrows;
-    private GameObject spikes;
     private Vector3 startingPos;                        //Stores object's starting pos for resetting
     public bool isActivated;                            //bool for when the pressure plate is activated
     private bool isLowered;                             //bool for whether the presure plate has been lowered
-    private bool spikesGenerated;                       //bool for whether the spikes have been risen or not
-    private float eventTimer;                           //the timer for how long the event should last
-    private float spikeHeight = 0.0f;
-    private int arrowIndex = 0;
     private float pressurePlateLowerAmount;
     #endregion
 
     // Use this for initialization
     void Start()
     {
-        arrows = new GameObject[arrowAmount];
         isActivated = false;
         isLowered = false;
-        spikesGenerated = false;
-        eventTimer = 0.0f;
         startingPos = gameObject.GetComponent<Transform>().position;
         lowerAmount = lowerAmount * 0.01f;
         lowerSpeed = lowerSpeed * 0.001f;
@@ -59,61 +45,6 @@ public class PressurePlate : MonoBehaviour
                     this.GetComponent<AudioSource>().Stop();
                 }
             }
-            #region Shoot Arrows Functionality
-            if (effect == EffectTypes.SHOOT_ARROWS_FROM_LEFT || effect == EffectTypes.SHOOT_ARROWS_FROM_RIGHT)
-            {
-                eventTimer += Time.deltaTime;
-                if (eventTimer >= 2.0f && arrowIndex < arrowAmount)  //continue to spawn arrows while we have less then specified
-                {
-                    SpawnArrow(arrowIndex);
-                    eventTimer = 0.0f;
-                    arrowIndex++;
-                }
-                for (int i = 0; i < arrowIndex; i++)
-                {
-                    if (effect == EffectTypes.SHOOT_ARROWS_FROM_LEFT)
-                    {
-                        arrows[i].transform.position = new Vector3(arrows[i].transform.position.x +
-                            (10.0f * Time.deltaTime), arrows[i].transform.position.y,
-                            arrows[i].transform.position.z);
-                    }
-                    if (effect == EffectTypes.SHOOT_ARROWS_FROM_RIGHT)
-                    {
-                        arrows[i].transform.position = new Vector3(arrows[i].transform.position.x -
-                            (10.0f * Time.deltaTime), arrows[i].transform.position.y,
-                            arrows[i].transform.position.z);
-                    }
-                }
-            }
-            #endregion
-            #region Spikes Functionality
-            else if (effect == EffectTypes.SPIKES)
-            {
-                if (!spikesGenerated)
-                {
-                    spikes = Instantiate(spikesPrefab);
-                    spikes.transform.position = new Vector3(this.transform.position.x - 0.7f,
-                       transform.position.y - 1.4f, transform.position.z + 0.6f);
-                    spikesGenerated = true;
-                }
-                if (spikesGenerated && spikeHeight <= 1.1f)
-                {
-                    eventTimer += Time.deltaTime;
-                    if (eventTimer >= 2.0f)
-                    {
-                        spikes.transform.position = new Vector3(spikes.transform.position.x,
-                            spikes.transform.position.y + (1.8f * Time.deltaTime), spikes.transform.position.z);
-                        spikeHeight += (1.8f * Time.deltaTime);
-                    }
-                }
-            }
-            #endregion
-            #region Open Door Functionality
-            else if (effect == EffectTypes.OPEN_DOOR)
-            {
-
-            }
-            #endregion
         }
     }
 
@@ -122,53 +53,23 @@ public class PressurePlate : MonoBehaviour
         if (other.tag == "Player" && isActivated == false)
         {
             isActivated = true;
+            GetComponent<AudioSource>().Play();
             Debug.Log("Presure Plate Activated");
-        }
-    }
-
-    private void SpawnArrow(int index)
-    {
-        arrows[index] = Instantiate(arrowPrefab);
-        if (effect == EffectTypes.SHOOT_ARROWS_FROM_LEFT)
-        {
-            arrows[index].transform.position = new Vector3(this.transform.position.x - 10.0f,
-                transform.position.y + 1.3f, transform.position.z);
-            arrows[index].transform.rotation = new Quaternion(0, 180, 0, 0);
-        }
-        else if (effect == EffectTypes.SHOOT_ARROWS_FROM_RIGHT)
-        {
-            arrows[index].transform.position = new Vector3(this.transform.position.x + 10.0f,
-                transform.position.y + 1.3f, transform.position.z);
         }
     }
 
     //Resets the presure plate and all of it's values for when the player respawns
     public void ResetPresurePlate()
     {
-        if (effect == EffectTypes.SHOOT_ARROWS_FROM_LEFT || effect == EffectTypes.SHOOT_ARROWS_FROM_RIGHT)
-        {
-            for (int i = 0; i < arrows.Length; i++)
-            {
-                Destroy(arrows[i]);
-            }
-
-            arrows = new GameObject[arrowAmount];
-            arrowIndex = 0;
-            Debug.Log("Arrows Reset.");
-        }
-        else if (effect == EffectTypes.SPIKES)
-        {
-            Destroy(spikes);
-            spikeHeight = 0.0f;
-            spikesGenerated = false;
-            Debug.Log("Spikes Reset.");
-        }
 
         isActivated = false;
         isLowered = false;
-        eventTimer = 0.0f;
         gameObject.GetComponent<Transform>().position = startingPos;
         gameObject.GetComponent<Collider>().enabled = true;
         lowerAmount = pressurePlateLowerAmount;
+        GetComponent<TriggerScript>().isActivated = false;
+        GetComponent<TriggerScript>().hasBeenActivatedAlready = false;
+        GetComponent<PressurePlate>().isActivated = false;
+        GetComponent<MeshCollider>().enabled = true;
     }
 }
