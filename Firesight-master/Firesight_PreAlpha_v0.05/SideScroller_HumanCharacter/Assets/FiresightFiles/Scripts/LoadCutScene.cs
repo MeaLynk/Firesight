a2b62a532bc15aea50497a972394129c4e2ef137
XPosterMaloneX <47110015+XPosterMaloneX@users.noreply.github.com>
2019-04-11 12:18:38 -0400

    updated

--- Firesight-master/Firesight_PreAlpha_v0.05/SideScroller_HumanCharacter/Assets/FiresightFiles/Scripts/LoadCutScene.cs
+++ Firesight-master/Firesight_PreAlpha_v0.05/SideScroller_HumanCharacter/Assets/FiresightFiles/Scripts/LoadCutScene.cs
@@ -1,26 +1,53 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadCutScene : MonoBehaviour
 {
     public int activeSceneIndex;
+    public Image fade;
+    public bool startFade = false;
+    public bool doneFading = false;
+
+    private GameObject player;
+
     // Use this for initialization
     void Start()
     {
         activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (startFade)
+        {
+            if (fade.color.a < 1)
+            {
+                fade.color = (fade.color) + (new Color(0.0f, 0.0f, 0.0f, 0.5f) * Time.deltaTime);
+            }
+            else
+            {
+                startFade = false;
+                doneFading = true;
+            }
+        }
+        if (doneFading)
+        {
+            SceneManager.LoadScene(activeSceneIndex + 1);
+        }
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            SceneManager.LoadScene(activeSceneIndex + 1);
-            SceneManager.UnloadSceneAsync(activeSceneIndex);
+            startFade = true;
+            player.GetComponent<PlayerMove>().canPlayerMove = false;
+            player.GetComponent<PlayerMove>().isPlayerInControl = false;
+            player.GetComponent<FireScript>().enabled = false;
+            GameObject.Find("Ozin").GetComponent<Animator>().Play("Run");
         }
     }
 }
