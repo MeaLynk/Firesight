ebba8246e0bcf79108d9c53c441ba54e706adbe2
XPosterMaloneX <47110015+XPosterMaloneX@users.noreply.github.com>
2019-04-14 20:29:41 -0400

    Intro Cutscene started

---
+++ Firesight-master/Firesight_PreAlpha_v0.05/SideScroller_HumanCharacter/Assets/FiresightFiles/Intro Scene Assets/Scripts/BreakGround.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BreakGround : MonoBehaviour
+{
+    public GameObject[] pieces;
+    [Range(1, 10)] public int maxFallSpeed;
+
+    private float[] fallSpeeds;
+    public bool isFalling = false;
+
+    // Use this for initialization
+    void Awake()
+    {
+        fallSpeeds = new float[pieces.Length];
+
+        for (int i = 0; i < pieces.Length; i++)
+        {
+            fallSpeeds[i] = Random.Range(15, maxFallSpeed + 15);
+        }
+        isFalling = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFalling)
+        {
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                pieces[i].transform.position = pieces[i].transform.position + new Vector3(0.0f, -fallSpeeds[i], 0.0f) * Time.deltaTime;
+                pieces[i].transform.Rotate((fallSpeeds[i] + 10) * Time.deltaTime, (fallSpeeds[i] + 10) * Time.deltaTime, (fallSpeeds[i] + 10) * Time.deltaTime);
+            }
+        }
+    }
+}
